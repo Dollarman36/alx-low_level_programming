d42b33c70d4acffddd0dd8c679ca9c212617d790
Dollarman36 <dollarmannnadi@.com>
2022-09-09 18:47:51 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- *  * main - Begin of execution
- *   *
- *    * Return: 0 if success
- *     */
+ * main - Begin of execution
+ * Return: 0 if success
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	return (0);
 }
