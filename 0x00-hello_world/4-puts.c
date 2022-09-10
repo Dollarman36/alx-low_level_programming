1b5257da38fa36435dcd4bcf2eb015ac528e3bcf
Dollarman36 <dollarmannnadi@.com>
2022-09-09 18:37:25 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+
+/**
+ *  * main - Begin of execution
+ *   *
+ *    * Return: 0 if success
+ *     */
+int main(void)
 {
-		//string initialisation
-		//    char Mystr[] = "C and C++";
-		//        
-		//            puts(Mystr); //writing the string to stdout
-		//                
-		//                    return 0;
-		//                    }
-		//
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+}
