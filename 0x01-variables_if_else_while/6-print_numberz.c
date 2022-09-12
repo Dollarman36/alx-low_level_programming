5d2109b155d03c4356e0e3af1d1f728e34754323
Dollarman36 <dollarmannnadi@.com>
2022-09-11 18:35:21 -0700

    base10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Begin of execution
+*
+* Return: 0 if success
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
