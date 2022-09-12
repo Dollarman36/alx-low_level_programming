a418c040481acbc6ccc4f619866ac45c4e2dda7c
Dollarman36 <dollarmannnadi@.com>
2022-09-11 18:47:17 -0700

    single number digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+
+/**
+* main - Begin of execution
+*
+* Return: 0 if success
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+
+		if (n < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+	putchar('\n');
+
+	return (0);
+}
