d94724e598eb9b7dab7f969acb6fbdd910e9c8c6
Dollarman36 <dollarmannnadi@.com>
2022-09-13 19:03:03 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <unistd.h>
 
 /**
@@ -8,7 +7,7 @@
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
 */
-int _putchar(char _putchar)
+int main()
 {
-	return (write(1, &_putchar, 1));
+	return (write(1, "_putchar\r\n", 11));
 }
