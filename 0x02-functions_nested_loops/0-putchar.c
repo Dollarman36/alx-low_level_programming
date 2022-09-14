fbbbf6f7147017c321fe438f7dea32621d3566b1
Dollarman36 <dollarmannnadi@.com>
2022-09-13 19:07:42 -0700

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,7 @@
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
 */
-int main()
+int main(void)
 {
-	return (write(1, "_putchar\r\n", 11));
+	return (write(1, "_putchar\n", 9));
 }
