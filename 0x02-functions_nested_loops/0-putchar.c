90aba17435aa25dd065416663c17ac255eab7c16
Dollarman36 <dollarmannnadi@.com>
2022-09-14 21:11:17 -0700

    p

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 /**
  * Main - Entry point
  *
@@ -10,8 +11,7 @@
 int main(void)
 {
 
-	_putchar('_putchar')
-		_putchar('\n');
+	printf("_putchar\n");
 
 	return (0);
 }
