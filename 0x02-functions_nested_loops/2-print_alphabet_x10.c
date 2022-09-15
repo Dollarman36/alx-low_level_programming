307c61dbdd4ec566e348eca7a030ff51c86a0d34
Dollarman36 <dollarmannnadi@.com>
2022-09-14 19:22:05 -0700

    alpha x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -print 10 times the alphabet
+ *
+ * return Always success
+ */
+void print_alphabet_x10(void)
+{
+
+	int n,co;
+	while (co < 10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+
+		}
+
+		co++;
+
+		_putchar('\n');
+	}
+}
