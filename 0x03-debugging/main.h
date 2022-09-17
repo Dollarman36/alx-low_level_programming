0978b020c92b3cf5e0d2807c87e6d61c4d47523a
Dollarman36 <dollarmannnadi@.com>
2022-09-16 19:03:34 -0700

    rema

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <sdio.h>
+
+#include <stdio.h>
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-void print_remaining_days(int day, int month, int year);
-int convert_day(int day, int month);
-#endif 
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
