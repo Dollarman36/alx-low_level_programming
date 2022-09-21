52ef6cd73352c05421237e86e3b59486fe04a85c
Dollarman36 <dollarmannnadi@.com>
2022-09-21 03:46:54 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef HOLBERTON_H
+#define HOLBERON_H
+
+int _putchar(char);
+char *_strcat(char*,char*);
+char *_strncat(char*,char*,int);
+char *_strncpy(char*,char*,int);
+int _strcmp(char*,char*);
+void reverse_array(int*,int);
+char *string_toupper(char *);
+void look_sep(char*, int*);
+int look_no_sep(char*, int*);
+char *cap_string(char*);
+int is_letter(char);
+char *leet(char*);
+char *rot13(char*);
+void print_number(int n);
+
+#endif
