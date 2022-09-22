88c1a3792647a20be914100713fdc20c023302e6
Dollarman36 <dollarmannnadi@.com>
2022-09-22 00:57:22 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,8 +2,8 @@
 
 /**
  * _strncat - concatenates two strings  up to n bytes from source
- * @dest: pointer to the destination to be concatenated
- * @src: point to source to be added to destination
+ *  @dest: pointer to the destination to be concatenated
+ *  @src: point to source to be added to destination
  * @n: number of bytes from source to be concatenated
  *
  * Return: pointer to destination
@@ -11,19 +11,17 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
+	int i = 0;
+	int j = 0;
 
-		int i = 0;
-		int j = 0;
+	while (dest[i] != '\0')
+		i++;
 
-		while (dest[i] != '\0')
-			i++;
-
-		while (j < n && src[j] != '\0')
-		{
-			dest[i] = src[j];
-			i++;
-			j++;
-		}
-
-		return (dest)
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
 }
